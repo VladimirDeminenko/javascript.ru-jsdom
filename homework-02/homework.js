db5b5339c8a53d974c009e7c23acf860009bed58
VladimirDeminenko <vladimir.deminenko+github@gmail.com>
2017-07-07 02:34:48 +0200

    homework-02

---
+++ homework-02/homework.js
@@ -0,0 +1,165 @@
+/**
+ * @file singleton.js
+ * @author Vladimir Deminenko
+ * @date 04.07.2017
+ */
+
+'use strict';
+
+const Singleton = (function () {
+    let instance = null;
+
+    return function () {
+        if (!instance) {
+            instance = this;
+        }
+
+        return instance;
+    }
+})();
+
+function isEmpty(obj) {
+    return Object.keys(obj).length === 0;
+}
+
+function sumOf(salaries) {
+    let result = 0;
+
+    for (let employee in salaries) {
+        result += salaries[employee];
+    }
+
+    return result;
+}
+
+function maxValue(salaries) {
+    let result = "нет сотрудников";
+    let max = 0;
+
+    for (let employee in salaries) {
+        if (salaries[employee] > max) {
+            max = salaries[employee];
+            result = employee;
+        }
+    }
+
+    return result;
+}
+
+function multiplyNumeric(obj) {
+    let value;
+
+    for (let prop in obj) {
+        value = obj[prop];
+
+        if (isNumeric(value)) {
+            obj[prop] = value * 2;
+        }
+    }
+
+    function isNumeric(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n)
+    }
+}
+
+function findInArray(arr, value) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === value) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+function filterRange(arr, a, b) {
+    let result = [];
+    let value;
+
+    for (let i = 0; i < arr.length; i++) {
+        value = arr[i];
+
+        if ((value >= a) && (value <= b)) {
+            result.push(value);
+        }
+    }
+
+    return result;
+}
+
+// Решето Эратосфена
+function primeNumbersTo(n) {
+    let p;
+    let result = initArray(n);
+    let idx = 0;
+
+    do {
+        p = result[idx];
+        result = strikeOut(result, idx);
+        idx++;
+    } while (p * p < n);
+
+    return result;
+
+
+    function strikeOut(arr, startIdx) {
+        let result = arr.slice(0, startIdx + 1); // starts as [2];
+        let p = arr[startIdx];
+        let value;
+
+        for (let i = startIdx + 1; i < arr.length; i++) {
+            value = arr[i];
+
+            if (value % p !== 0) {
+                result.push(value);
+            }
+        }
+
+        return result;
+    }
+
+    function initArray(n) {
+        let result = [];
+
+        for (let i = 2; i < n; i++) {
+            result.push(i);
+        }
+
+        return result;
+    }
+}
+
+function sumOfPrimeNumbersTo(n) {
+    let result = 0;
+    let primeNumbers = primeNumbersTo(n);
+
+    for (let i = 0; i < primeNumbers.length; i++) {
+        result += primeNumbers[i];
+    }
+
+    return result;
+}
+
+function getMaxSubSum(arr) {
+    let result = 0;
+    let subSum = 0;
+    let value;
+
+    for (let i = 0; i < arr.length; i++) {
+        value = arr[i];
+
+        if (value < 0) {
+            subSum = 0;
+
+            continue;
+        }
+
+        subSum += value;
+
+        if (subSum > result) {
+            result = subSum;
+        }
+    }
+
+    return result;
+}
---
+++ homework-02/homeworkTest.js
@@ -0,0 +1,181 @@
+/**
+ * @file singletonTest.js
+ * @author Vladimir Deminenko
+ * @date 04.07.2017
+ */
+
+describe("Singleton", function () {
+    it("new Singleton() === new Singleton()", function () {
+        assert.strictEqual(new Singleton(), new Singleton());
+    });
+});
+
+describe("Пуст ли объект", function () {
+    it("returns true for an empty object", function () {
+        assert.isTrue(isEmpty({}));
+    });
+
+    it("returns false if a property exists", function () {
+        assert.isFalse(isEmpty({
+            anything: false
+        }));
+    });
+});
+
+describe("Сумма свойств", function () {
+    it("returns sum of salaries", function () {
+        const salaries = {
+            "Вася": 100,
+            "Петя": 300,
+            "Даша": 250
+        };
+
+        assert.equal(650, sumOf(salaries));
+    });
+
+    it("returns 0", function () {
+        assert.equal(0, sumOf({}));
+    });
+});
+
+describe("Свойство с наибольшим значением", function () {
+    it("returns имя сотрудника, у которого самая большая зарплата", function () {
+        const salaries = {
+            "Вася": 100,
+            "Петя": 300,
+            "Даша": 250
+        };
+
+        assert.equal("Петя", maxValue(salaries));
+    });
+
+    it("returns \"нет сотрудников\"", function () {
+        assert.equal("нет сотрудников", maxValue({}));
+    });
+});
+
+describe("multiplyNumeric", function () {
+    it("multiplies all numeric properties by 2", function () {
+        let menu = {
+            width: 200,
+            height: 300,
+            title: "My menu"
+        };
+
+        multiplyNumeric(menu);
+
+        assert.equal(menu.width, 400);
+        assert.equal(menu.height, 600);
+        assert.equal(menu.title, "My menu");
+    });
+
+    it("returns nothing", function () {
+        assert.isUndefined(multiplyNumeric({}));
+    });
+
+});
+
+describe("findInArray", function () {
+
+    describe("возвращает позицию, на которой найден элемент", function () {
+        it("в массиве [1,2,3] находит 1 на позиции 0", function () {
+            assert.equal(findInArray([1, 2, 3], 1), 0);
+        });
+        it("в массиве [1,2,3] находит 2 на позиции 1", function () {
+            assert.equal(findInArray([1, 2, 3], 2), 1);
+        });
+        it("в массиве [1,2,3] находит 3 на позиции 2", function () {
+            assert.equal(findInArray([1, 2, 3], 3), 2);
+        });
+    });
+
+    it("если элемент не найден, возвращает -1", function () {
+        assert.equal(findInArray([1, 2, 3], 0), -1);
+    });
+
+    it("отличает false или null от 0", function () {
+        assert.equal(findInArray([false, true, null], 0), -1);
+    });
+
+    it("отличает 1 от true", function () {
+        assert.equal(findInArray([1, 2, 3], true), -1);
+    });
+});
+
+describe("filterRange", function () {
+
+    it("returns the filtered values", function () {
+
+        let arr = [5, 3, 8, 1];
+
+        let filtered = filterRange(arr, 1, 4);
+
+        assert.deepEqual(filtered, [3, 1]);
+    });
+
+    it("doesn't change the array", function () {
+
+        let arr = [5, 3, 8, 1];
+
+        let filtered = filterRange(arr, 1, 4);
+
+        assert.deepEqual(arr, [5, 3, 8, 1]);
+    });
+
+});
+
+describe("Решето Эратосфена", function () {
+
+    it("returns все простые числа до 100", function () {
+
+        let arr = [
+            2, 3, 5, 7, 11,
+            13, 17, 19, 23,
+            29, 31, 37, 41,
+            43, 47, 53, 59,
+            61, 67, 71, 73,
+            79, 83, 89, 97
+        ];
+
+        assert.deepEqual(arr, primeNumbersTo(100));
+    });
+
+    it("returns сумму всех простых чисел до 100", function () {
+        assert.deepEqual(1060, sumOfPrimeNumbersTo(100));
+    });
+
+});
+
+describe("getMaxSubSum", function() {
+    it("maximal subsum of [1, 2, 3] equals 6", function() {
+        assert.equal(6, getMaxSubSum([1, 2, 3]));
+    });
+
+    it("maximal subsum of [-1, 2, 3, -9] equals 5", function() {
+        assert.equal(5, getMaxSubSum([-1, 2, 3, -9]));
+    });
+
+    it("maximal subsum of [-1, 2, 3, -9, 11] equals 11", function() {
+        assert.equal(11, getMaxSubSum([-1, 2, 3, -9, 11]));
+    });
+
+    it("maximal subsum of [-2, -1, 1, 2] equals 3", function() {
+        assert.equal(3, getMaxSubSum([-2, -1, 1, 2]));
+    });
+
+    it("maximal subsum of [100, -9, 2, -3, 5] equals 100", function() {
+        assert.equal(100, getMaxSubSum([100, -9, 2, -3, 5]));
+    });
+
+    it("maximal subsum of [] equals 0", function() {
+        assert.equal(0, getMaxSubSum([]));
+    });
+
+    it("maximal subsum of [-1] equals 0", function() {
+        assert.equal(0, getMaxSubSum([-1]));
+    });
+
+    it("maximal subsum of [-1, -2] equals 0", function() {
+        assert.equal(0, getMaxSubSum([-1, -2]));
+    });
+});
