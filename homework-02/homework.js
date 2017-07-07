ce909af000651c5aa43bc562a4febfbc38809d9e
VladimirDeminenko <vladimir.deminenko+github@gmail.com>
2017-07-07 18:46:24 +0200

    homework-02

--- homework-02/homework.js
+++ homework-02/homework.js
@@ -121,7 +121,7 @@ function primeNumbersTo(n) {
     function initArray(n) {
         let result = [];
 
-        for (let i = 2; i < n; i++) {
+        for (let i = 2; i <= n; i++) {
             result.push(i);
         }
 
@@ -148,6 +148,126 @@ function getMaxSubSum(arr) {
         subSum = Math.max(0, subSum + arr[i]);
         result = Math.max(subSum, result);
     }
+
+    return result;
+}
+
+function addClass(obj, cls) {
+    let classes = obj.className.split(' ');
+
+    if (classes.indexOf(cls)) {
+        classes.push(cls);
+    }
+
+    obj.className = classes.join(' ').trim();
+}
+
+function removeClass(obj, cls) {
+    let classes = obj.className.split(' ');
+    let idx;
+    let isFound;
+
+    do {
+        idx = classes.indexOf(cls);
+        isFound = !!~idx;
+
+        if (isFound) {
+            classes.splice(idx, 1);
+        }
+
+    } while (isFound);
+
+
+    obj.className = classes.join(' ');
+}
+
+function camelize(str) {
+    let word;
+    let words = str.split('-');
+
+    for (let i = 1; i < words.length; i++) {
+        word = words[i];
+        word = `${word.charAt(0).toUpperCase()}${word.slice(1)}`;
+        words[i] = word;
+    }
+
+    return words.join('');
+}
+
+function filterRangeInPlace(arr, a, b) {
+    let value;
+    let idx = 0;
+
+    while (idx < arr.length) {
+        value = arr[idx];
+
+        if ((value >= a) && (value <= b)) {
+            idx++;
+        }
+        else {
+            arr.splice(idx, 1);
+        }
+    }
+}
+
+function aclean(arr) {
+    let result = [];
+    let keys = [];
+    let key;
+    let firstKeyIdx;
+    let lastKeyIdx;
+    let word;
+
+    for (let i = 0; i < arr.length; i++) {
+        word = arr[i];
+
+        key = createKey(word);
+        keys.push(key);
+        firstKeyIdx = keys.indexOf(key);
+        lastKeyIdx = keys.lastIndexOf(key);
+
+        if ((keys.indexOf(key, firstKeyIdx + 1) === lastKeyIdx) && !!~firstKeyIdx) {
+            result.push(word);
+        }
+    }
+
+    return result;
+
+    function createKey(word) {
+        return word
+            .trim()
+            .toLowerCase()
+            .split('')
+            .sort()
+            .join('');
+    }
+}
+
+function unique(arr) {
+    let result = [];
+    let value;
+
+    for (let i = 0; i < arr.length; i++) {
+        value = arr[i];
+
+        if (!~result.indexOf(value)) {
+            result.push(value);
+        }
+    }
+
+    return result;
+}
+
+function getSums(arr) {
+    let result = [];
+
+    arr.reduce(function (previousValue, currentItem) {
+        let sum = +previousValue + +currentItem;
+
+        result.push(sum);
+
+        return sum;
+    }, 0);
 
     return result;
 }
