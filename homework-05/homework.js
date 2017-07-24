460261c81cb386687c788d58c3f61ed43fe45324
VladimirDeminenko <vladimir.deminenko+github@gmail.com>
2017-07-24 03:00:09 +0200

    homework-05

--- homework-05/homework.js
+++ homework-05/homework.js
@@ -129,3 +129,30 @@ function makeCaching(f) {
         return result;
     }
 }
+
+function CoffeeMachine(power) {
+    const WATER_HEAT_CAPACITY = 4200;
+
+    let self = this;
+    let timerId = null;
+
+    self.waterAmount = 0;
+
+    function getBoilTime() {
+        return self.waterAmount * WATER_HEAT_CAPACITY * 80 / power;
+    }
+
+    function onReady() {
+        console.log('Кофе готов!');
+    }
+
+    self.stop = function () {
+        if (timerId) {
+            clearTimeout(timerId);
+        }
+    };
+
+    self.run = function () {
+        timerId = setTimeout(onReady, getBoilTime());
+    };
+}
