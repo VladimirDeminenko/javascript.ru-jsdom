f966c571975adfda2dcf8d0b90c5e2b4be1a2f98
VladimirDeminenko <vladimir.deminenko+github@gmail.com>
2017-08-10 22:41:56 +0200

    homework-03

--- homework-03/deepEqual.js
+++ homework-03/deepEqual.js
@@ -8,35 +8,34 @@
 
 /**
  * function deepEqual() compares two objects and their properties
- * @param {Object} obj1
+ * @param {Object} obj
  * @param {Object} obj2
  * @returns {boolean}
  */
-function deepEqual(obj1, obj2) {
+function deepEqual(obj, obj2) {
     // сравнение примитивов, отличных от undefined
-    if (![typeof obj1, typeof obj2].includes('object')) {
-        return obj1 === obj2 && obj1 !== undefined;
+    if (![typeof obj, typeof obj2].includes('object')) {
+        return obj === obj2 && obj !== undefined;
     }
 
     if (
-        obj1 === null &&
+        obj === null &&
         obj2 === null
     ) {
         return true;
     }
 
-    let keys;
-    let keys2;
-
-    // использование try...catch
-    // позволяет обойтись без многих проверок - сравни с deepEqual2()
-    try {
-        keys = Object.keys(obj1);
-        keys2 = Object.keys(obj2);
-    }
-    catch (err) {
+    if (
+        typeof obj !== 'object' ||
+        typeof obj2 !== 'object' ||
+        obj === null ||
+        obj2 === null
+    ) {
         return false;
     }
+
+    let keys = Object.keys(obj);
+    let keys2 = Object.keys(obj2);
 
     if (keys.length !== keys2.length) {
         return false;
@@ -44,66 +43,14 @@ function deepEqual(obj1, obj2) {
 
     for (let i = 0; i < keys.length; i++) {
         let key = keys[i];
+        let value = obj[key];
+        let value2 = obj2[key];
 
-        if (typeof obj1[key] === 'object') {
-            return deepEqual(obj1[key], obj2[key]);
+        if (typeof value === 'object') {
+            return deepEqual(value, value2);
         }
 
-        if (obj1[key] !== obj2[key]) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-/**
- * function deepEqual() compares two objects and their properties
- * @param {Object} obj1
- * @param {Object} obj2
- * @returns {boolean}
- */
-function deepEqual2(obj1, obj2) {
-    // сравнение примитивов, отличных от undefined
-    if (![typeof obj1, typeof obj2].includes('object')) {
-        return obj1 === obj2 && obj1 !== undefined;
-    }
-
-    if (
-        obj1 === null &&
-        obj2 === null
-    ) {
-        return true;
-    }
-
-    if (
-        typeof obj1 !== 'object' ||
-        typeof obj2 !== 'object' ||
-        (obj1 === null && obj2 !== null) ||
-        (obj1 !== null && obj2 === null)
-    ) {
-        return false;
-    }
-
-    let keys = Object.keys(obj1);
-    let keys2 = Object.keys(obj2);
-
-    if (
-        keys && keys.length &&
-        keys2 && keys2.length &&
-        keys.length !== keys2.length
-    ) {
-        return false;
-    }
-
-    for (let i = 0; i < keys.length; i++) {
-        let key = keys[i];
-
-        if (typeof obj1[key] === 'object') {
-            return deepEqual(obj1[key], obj2[key]);
-        }
-
-        if (obj1[key] !== obj2[key]) {
+        if (value !== value2) {
             return false;
         }
     }
